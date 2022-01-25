6eb32c5f3cc8e2f16ed4e93cd8170d5c4014a854
iDolphin99 <idolphin.kr@gmail.com>
2022-01-25 23:58:20 +0900

    chapter 2 ing

--- Cpp_Study/02_4_1.cpp
+++ Cpp_Study/02_4_1.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 int main(void)
 {
-	char* str1 = new char[20];
-	char* str2 = new char[20];
-
-	str1 = "I am";
-	str2 = "AI";
+	char *str1 = "ABC 123 ";
+	char *str2 = "AI";
 
 	cout << "strlen: " << strlen(str1) << endl;
-	cout << "strcat: " << strcat(str1, str2) << endl;
-	cout << "strcpy: " << strcpy(str1, str2) << endl;
-	cout << "strcmp: " << strcmp(str1, str2) << endl;
+	//cout << "strcat: " << strcat(str1, str2) << endl;
+	//cout << "strcpy: " << strcpy(str1, str2) << endl;
+	//cout << "strcmp: " << strcmp(str1, str2) << endl;
 
 	return 0;
 }
